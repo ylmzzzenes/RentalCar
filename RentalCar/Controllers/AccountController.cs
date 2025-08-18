cd88c34e5b3ab3b00b3ecf6fa20dbe3423a5cb88
Enes Yılmaz <enesyilmaz.ey3@gmail.com>
2025-08-19 00:24:11 +0300

    Identity'yi proje eklemeye başladım.AccountController'da Create metodunu yazdım.RentalCar içine CreatWiewModel'i ekledim.

---
+++ RentalCar/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using RentalCar.Data.Models;
+using RentalCar.ViewModels;
+
+namespace RentalCar.Controllers
+{
+    public class AccountController : Controller
+    {
+        private RoleManager<AppRole> _roleManager;
+        private UserManager<AppUser> _userManager;
+        private SignInManager<AppUser> _signInManager;
+       
+
+        public AccountController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        { 
+            return View();
+        }
+        [HttpPost]
+        
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser
+                {
+                    UserName=model.UserName,
+                    Email=model.Email,
+                    FullName = model.FullName
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ RentalCar/ViewModels/CreateViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentalCar.ViewModels
+{
+    public class CreateViewModel
+    {
+        [Required]
+        public string? FullName { get; set; }
+        [Required]
+        public string? UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Parola Eşleşmiyor")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
