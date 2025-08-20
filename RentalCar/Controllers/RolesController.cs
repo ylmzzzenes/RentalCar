44635a311a9fcb8c5e6a8ab64d55b03cf33fee47
Enes Yılmaz <enesyilmaz.ey3@gmail.com>
2025-08-21 01:58:41 +0300

    Kendimiz TagHelpers oluşturduk ve RolesContorller'a edit metodu ekledik , View'da da karşılığını sağlamış olduk.

--- RentalCar/Controllers/RolesController.cs
+++ RentalCar/Controllers/RolesController.cs
@@ -40,5 +40,47 @@ namespace RentalCar.Controllers
             }
             return View(model);
         }
+
+        public async Task<IActionResult> Edit (string id)
+        {
+            var role=await _roleManager.FindByIdAsync(id);
+            if(role !=null && role.Name != null)
+            {
+                ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+                return View(role);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AppRole model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role=await _roleManager.FindByIdAsync(model.Id);
+                if (role!=null)
+                {
+                    role.Name = model.Name;
+                    var result = await _roleManager.UpdateAsync(role);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+
+                    }
+                    foreach(var err in result.Errors)
+                    {
+                        ModelState.AddModelError("",err.Description); 
+                    }
+                    if(role.Name != null)
+                    {
+                        ViewBag.Users=await _userManager.GetUsersInRoleAsync(role.Name);
+                    }
+
+                }
+              
+            }
+            return View(model);
+        }
     }
 }
