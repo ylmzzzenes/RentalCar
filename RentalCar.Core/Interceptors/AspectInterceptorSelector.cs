036ef2343adb0767afe45be66372b5313151ebc1
Enes Yılmaz <enes213132@ogr.duzce.edu.tr>
2026-04-14 00:27:56 +0300

    feat(core): add interceptor infrastructure with Castle DynamicProxy

---
+++ RentalCar.Core/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalCar.Core.Interceptors
+{
+    internal class AspectInterceptorSelector
+    {
+    }
+}
---
+++ RentalCar.Core/Interceptors/MethodInterception.cs
@@ -0,0 +1,53 @@
+﻿using Castle.DynamicProxy;
+
+namespace RentalCar.Core.Interceptors
+{
+    public abstract class MethodInterception: MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation)
+        {
+
+        }
+
+        protected virtual void onAfter(IInvocation invocation)
+        {
+
+        }
+        protected virtual void onException(IInvocation invocation, Exception exception)
+        {
+
+        }
+
+        protected virtual void onSuccess(IInvocation ınvocation)
+        {
+
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+
+            OnBefore(invocation);
+
+            try
+            {
+                invocation.Proceed();
+            }
+            catch(Exception ex)
+            {
+                isSuccess = false;
+                onException(invocation, ex);
+                throw;
+            }
+            finally
+            {
+                if(isSuccess)
+                {
+                    onSuccess(invocation);
+                }
+
+                onAfter(invocation);
+            }
+        }
+    }
+}
